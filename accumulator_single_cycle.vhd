214ffcb6f67246c86ada1067c37062f1c718acae
gabriel costa <gabriel.costa@ua.pt>
2024-12-21 21:11:54 +0000

    changed some variables' names

--- accumulator_single_cycle.vhd
+++ accumulator_single_cycle.vhd
@@ -22,26 +22,26 @@ end accumulator;
 
 
 architecture structural of accumulator is
-  signal delayed_write_addr : std_logic_vector(ADDR_BITS-1 downto 0);
-  signal delayed_write_inc,value_to_write, s_aux_r_d:  std_logic_vector(2**DATA_BITS_LOG2-1 downto 0);
+  signal s_write_addr_stable : std_logic_vector(ADDR_BITS-1 downto 0);
+  signal s_write_inc_stable,s_value_to_write, s_aux_r_d:  std_logic_vector(2**DATA_BITS_LOG2-1 downto 0);
 begin
     my_register_addr: entity work.my_register(behavioral)
                  generic map(N => ADDR_BITS)
                     port map(clock => clock,
                               d     => write_addr,
-                              q     => delayed_write_addr);
+                              q     => s_write_addr_stable);
 
     -- my_register_addr2: entity work.my_register(behavioral)
     --                    generic map(N => ADDR_BITS)
     --                       port map(clock => clock,
-    --                                d     => delayed_write_addr,
+    --                                d     => s_write_addr_stable,
     --                                q     => delayed_write_addr2);
 
     my_register_inc:  entity work.my_register(behavioral)
                       generic map(N => 2**DATA_BITS_LOG2)
                          port map(clock => clock,
                                   d     => write_inc,
-                                  q     => delayed_write_inc);
+                                  q     => s_write_inc_stable);
 
 
 
@@ -49,10 +49,10 @@ begin
   -- begin
   --   if rising_edge(clock) then
   --     if write_inc_changed = '1' then
-  --       -- adder_n executes and value_to_write gets updated
-  --       value_to_write <= value_to_write; -- Signal propagation depends on this
+  --       -- adder_n executes and s_value_to_write gets updated
+  --       s_value_to_write <= s_value_to_write; -- Signal propagation depends on this
   --     else
-  --       value_to_write <= value_to_write; -- Optional: Hold previous value
+  --       s_value_to_write <= s_value_to_write; -- Optional: Hold previous value
   --     end if;
   --   end if;
   -- end process;
@@ -60,34 +60,34 @@ begin
                       generic map(ADDR_BITS => ADDR_BITS, DATA_BITS_LOG2 => DATA_BITS_LOG2)
                          port map(clock         => clock,
                                   write_en      => '1',
-                                  write_addr    => delayed_write_addr,
-                                  write_data    => value_to_write,
+                                  write_addr    => s_write_addr_stable,
+                                  write_data    => s_value_to_write,
                                   
                                   read_addr     => read_addr,
                                   read_data     => read_data,
-                                  aux_read_addr => delayed_write_addr,
+                                  aux_read_addr => s_write_addr_stable,
                                   aux_read_data => s_aux_r_d);
 
 
     adder_n:  entity work.adder_n(structural)
     generic map(N => 2**DATA_BITS_LOG2)
         port map(a     => s_aux_r_d,
-                b     => delayed_write_inc,
+                b     => s_write_inc_stable,
                 c_in  => '0',
-                s     => value_to_write,
+                s     => s_value_to_write,
                 c_out  => open);
     
-    -- process(value_to_write) is
+    -- process(s_value_to_write) is
     -- begin
-    --   if(delayed_write_inc = write_inc) then
-    --     value_to_write <= s_aux_r_d;
+    --   if(s_write_inc_stable = write_inc) then
+    --     s_value_to_write <= s_aux_r_d;
     --   end if;
     -- end process;
-    -- delayed_write_inc <= (others => '0');
+    -- s_write_inc_stable <= (others => '0');
 
-  -- process(value_to_write) is
+  -- process(s_value_to_write) is
   -- begin
-  --   value_to_write <= (others => '0');
+  --   s_value_to_write <= (others => '0');
   -- end process;
 end structural;
 
